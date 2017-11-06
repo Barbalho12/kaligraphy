ecccef8becbbcd81d40d20894fda650fcb261237
Felipe Barbalho <felipebarbalho.95@gmail.com>
2017-11-05 22:52:43 -0300

    First commit

---
+++ app/src/main/java/com/barreto/kaligraphy/MainActivity.java
@@ -0,0 +1,126 @@
+package com.barreto.kaligraphy;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.MotionEvent;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    TextView tv_information;
+    TouchEventView tev;
+    ImageView iv_source;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+
+        tv_information = (TextView) findViewById(R.id.tv_information);
+        tev = (TouchEventView) findViewById(R.id.tev_animation);
+        iv_source = (ImageView) findViewById(R.id.iv_source);
+        tev.setTv(tv_information);
+
+        int width = iv_source.getDrawable().getIntrinsicWidth();
+        int height = iv_source.getDrawable().getIntrinsicHeight();
+
+        tv_information.setText("Height: "+height+", Width: "+width);
+
+
+        iv_source.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                float eventX = event.getX();
+                float eventY = event.getY();
+
+
+                tv_information.setText("X: " + eventX+", Y: "+eventY);
+
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/barreto/kaligraphy/TouchEventView.java
@@ -0,0 +1,105 @@
+package com.barreto.kaligraphy;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+public class TouchEventView extends View {
+    private Paint paint = new Paint();
+    private Path path = new Path();
+    Context context;
+
+    public TouchEventView(final Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+
+        paint.setAntiAlias(true);
+        paint.setStrokeWidth(10f);
+        paint.setColor(Color.RED);
+
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+
+
+//        setOnLongClickListener(new OnLongClickListener() {
+//            @Override
+//            public boolean onLongClick(View v) {
+//                paint.reset();
+//                Toast.makeText(context, "Long Clicked", Toast.LENGTH_SHORT).show();
+//                return true;
+//            }
+//        });
+    }
+
+    TextView tv = null;
+
+    public void setTv(TextView tv){
+        this.tv = tv;
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        paint.setColor(Color.RED);
+        canvas.drawPath(path, paint);
+        paint.setColor(Color.BLUE);
+
+
+
+        canvas.drawCircle(240, 60, 20,  paint);
+        canvas.drawCircle(180, 220, 20,  paint);
+        canvas.drawCircle(105, 350, 20,  paint);
+        canvas.drawCircle(20, 450, 20,  paint);
+
+        canvas.drawCircle(195, 280, 20,  paint);
+        canvas.drawCircle(300, 390, 20,  paint);
+        canvas.drawCircle(380, 470, 20,  paint);
+        canvas.drawCircle(490, 500, 20,  paint);
+
+        paint.setColor(Color.GREEN);
+        canvas.drawLine(240, 60, 180, 220, paint);
+        canvas.drawLine(180, 220, 105, 350, paint);
+        canvas.drawLine(105, 350, 20, 450, paint);
+
+        canvas.drawLine(195, 280, 300, 390, paint);
+        canvas.drawLine(300, 390, 380, 470, paint);
+        canvas.drawLine(380, 470, 490, 500, paint);
+    }
+
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float eventX = event.getX();
+        float eventY = event.getY();
+
+        if(tv != null){
+            tv.setText("X: " + eventX/512+", Y: "+eventY/512);
+        }
+
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(eventX, eventY);
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                path.lineTo(eventX, eventY);
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            default:
+                return false;
+        }
+
+        // Schedules a repaint.
+        invalidate();
+        return true;
+    }
+}
